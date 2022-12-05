a4314d34dbf3a8f5f8a66aba7693faf8b09384e0
Berkley, Shimi (sb2144) <sb2144@intl.att.com>
2022-12-05 12:16:25 +0200

    remove tabindex entirely rather than assigning -1in case of no overflow

--- src/hooks/use-auto-overflow.ts
+++ src/hooks/use-auto-overflow.ts
@@ -4,7 +4,11 @@ import { isOverflown } from '../utils/dom.utils';
 export const useAutoOverflow = (ref: RefObject<HTMLElement>) => {
   const keyListener = useCallback((event: KeyboardEvent) => {
     if (event.key === 'Tab' && ref.current) {
-      ref.current.tabIndex = isOverflown(ref.current) ? 0 : -1;
+      if (isOverflown(ref.current)) {
+        ref.current.setAttribute('tabindex', '0');
+      } else {
+        ref.current.removeAttribute('tabindex');
+      }
     }
   }, []);
 
