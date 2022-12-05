e33d9b8fec4e7d6b4f42e8c9b2b2084566e6ab42
Berkley, Shimi (sb2144) <sb2144@intl.att.com>
2022-12-05 14:09:59 +0200

    add missing dependencies

--- src/hooks/use-auto-overflow.ts
+++ src/hooks/use-auto-overflow.ts
@@ -2,15 +2,18 @@ import { RefObject, useCallback, useEffect } from 'react';
 import { isOverflown } from '../utils/dom.utils';
 
 export function useAutoOverflow(ref: RefObject<HTMLElement>) {
-  const keyListener = useCallback((event: KeyboardEvent) => {
-    if (event.key === 'Tab' && ref.current) {
-      if (isOverflown(ref.current)) {
-        ref.current.setAttribute('tabindex', '0');
-      } else {
-        ref.current.removeAttribute('tabindex');
+  const keyListener = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Tab' && ref.current) {
+        if (isOverflown(ref.current)) {
+          ref.current.setAttribute('tabindex', '0');
+        } else {
+          ref.current.removeAttribute('tabindex');
+        }
       }
-    }
-  }, []);
+    },
+    [ref],
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', keyListener);
@@ -18,5 +21,5 @@ export function useAutoOverflow(ref: RefObject<HTMLElement>) {
     return () => {
       document.removeEventListener('keydown', keyListener);
     };
-  }, []);
+  }, [keyListener]);
 }
