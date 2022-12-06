7211b24283969c44657952f536c6a3218d7c3e41
Berkley, Shimi (sb2144) <sb2144@intl.att.com>
2022-12-06 22:41:16 +0200

    set initial tabindex so it can be used via css even before a tab keypress

--- src/hooks/use-auto-overflow.ts
+++ src/hooks/use-auto-overflow.ts
@@ -2,24 +2,22 @@ import { RefObject, useCallback, useEffect } from 'react';
 import { isOverflown } from '../utils/dom.utils';
 
 export function useAutoOverflow(ref: RefObject<HTMLElement>) {
-  const setTabIndex = useCallback(() => {
+  const setTabIndex = () => {
     if (ref.current) {
       ref.current.tabIndex = isOverflown(ref.current) ? 0 : -1;
     }
-  }, [ref]);
+  };
 
+  // set the initial tabindex
   useEffect(() => {
     setTabIndex();
-  }, [setTabIndex]);
+  }, []);
 
-  const keyListener = useCallback(
-    (event: KeyboardEvent) => {
-      if (event.key === 'Tab') {
-        setTabIndex();
-      }
-    },
-    [setTabIndex],
-  );
+  const keyListener = useCallback((event: KeyboardEvent) => {
+    if (event.key === 'Tab') {
+      setTabIndex();
+    }
+  }, []);
 
   useEffect(() => {
     document.addEventListener('keydown', keyListener);
